7ad4cfd9114ecebdc73f08527508158ab56a28c0
Andres Gonzalez <agonza40@gmail.com>
2017-11-08 22:42:23 -0800

    Rename lengthy classes

--- objects.ts
+++ objects.ts
@@ -23,27 +23,28 @@ interface Point3D extends Point2D {
     z: number;
 }
 
-class OneLengthyBoi implements Lengthy {
+class LengthyClass implements Lengthy {
     constructor (public length: number = 0) {}
 }
 
-class AnotherLengthyBoi {
+class AnotherLengthyClass {
     get length():number {
         return this.str.length;
     }
     constructor (public readonly str:string) {}
 }
 
-interface AnotherLengthyBoiInterface extends AnotherLengthyBoi {}
+interface AnotherLengthyInterface extends AnotherLengthyClass {}
 // Extracts the classes interface and leaves the implementation behind
 // This would be the same as:
-interface AnotherLengthyBoiInterface {
+interface AnotherLengthyInterface {
     length: number;
     readonly str: string;
 }
 
 // To make a hash object: {[index: string | number]: }
 type NumberHash = {[index: string]: number};
+type Indexable = {[index:number]: string};
 
 interface OptionalProperties {
     notOptionalProp: number;
